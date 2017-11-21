9f7a3461847f3d8beddf8cfac3ccdc66e67d776c
Álvaro Galán <alvaro.galan@solucionex.com>
2017-11-21 15:11:14 +0100

    Tests

---
+++ src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace AppBundle\Tests\Controller\Api;
+
+use AppBundle\Test\ApiTestCase;
+
+class ProgrammerController extends ApiTestCase
+{
+    public function setup()
+    {
+        parent::setup();
+
+        $this->createUser('weaverryan');
+    }
+
+    public function testPOST()
+    {
+        $data = array(
+            'nickname' => 'ObjectOrienter',
+            'avatarNumber' => 5,
+            'tagLine' => 'a test dev!'
+        );
+
+        // 1) POST to create a programmer
+        $response = $this->client->post('/api/programmers', [
+            'body' => json_encode($data)
+        ]);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertStringEndsWith('/api/programmers/ObjectOrienter', $response->getHeader('Location'));
+        $finishedData = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('nickname', $finishedData);
+        $this->assertEquals('ObjectOrienter', $data['nickname']);
+    }
+
+    public function testGETProgrammer()
+    {
+        $this->createProgrammer(array(
+            'nickname' => 'UnitTester',
+            'avatarNumber' => 3,
+        ));
+
+        $response = $this->client->get('/api/programmers/UnitTester');
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertiesExist($response, array(
+            'nickname',
+            'avatarNumber',
+            'powerLevel',
+            'tagLine'
+        ));
+        $this->asserter()->assertResponsePropertyEquals($response, 'nickname', 'UnitTester');
+    }
+
+    public function testGETProgrammersCollection()
+    {
+        $this->createProgrammer(array(
+            'nickname' => 'UnitTester',
+            'avatarNumber' => 3,
+        ));
+
+        $this->createProgrammer(array(
+            'nickname' => 'CowboyCoder',
+            'avatarNumber' => 5,
+        ));
+
+        $response = $this->client->get('/api/programmers');
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertyIsArray($response, 'programmers');
+        $this->asserter()->assertResponsePropertyCount($response, 'programmers', 2);
+        $this->asserter()->assertResponsePropertyEquals($response, 'programmers[1].nickname', 'CowboyCoder');
+    }
+}
--- testing.php
+++ testing.php
@@ -9,7 +9,25 @@ $client = new \GuzzleHttp\Client([
     ]
 ]);
 
-$response = $client->post('/api/programmers');
+$nickname = 'ObjectOrienter'.rand(0, 999);
+$data = array(
+    'nickname' => $nickname,
+    'avatarNumber' => 5,
+    'tagLine' => 'a test dev!'
+);
+
+// 1) POST to create a programmer
+$response = $client->post('/api/programmers', [
+    'body' => json_encode($data)
+]);
+
+$programmerUrl = $response->getHeader('Location');
+
+// 2) GET to fetch that programmer
+$response = $client->get($programmerUrl);
+
+// 3) GET a collection
+$response = $client->get('/api/programmers');
 
 echo $response;
 echo "\n\n";
